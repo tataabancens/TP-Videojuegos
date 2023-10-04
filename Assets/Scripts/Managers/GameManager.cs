f353b8b825b04980a4884d51fd9caee9d8f6c836
GonzaloRossin <gonzarossin@gmail.com>
2023-10-04 18:05:25 -0300

    feat: points counter working

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,9 +8,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private bool _isGameOver = false;
     [SerializeField] private bool _isVictory = false;
     [SerializeField] private float _timerInSeconds = 30f;
-    private int _pointsCounter = 0;
+    private int _points = 0;
     [SerializeField] private TextMeshProUGUI _gameOverMessage;
     [SerializeField]private TextMeshProUGUI _timerCounter;
+    [SerializeField] private TextMeshProUGUI _pointsCounter;
 
     #region UNITY_EVENTS
     // Start is called before the first frame update
@@ -23,25 +24,8 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if(_timerInSeconds > 0)
-        {
-            _timerInSeconds -= Time.deltaTime;
-        }
-        else if(_timerInSeconds < 0)
-        {
-            _timerInSeconds = 0;
-            EventsManager.instance.EventGameOver(true);
-        }
-        int minutes = Mathf.FloorToInt(_timerInSeconds / 60);
-        int seconds = Mathf.FloorToInt(_timerInSeconds % 60);
-        if(_timerInSeconds < 30 && _timerInSeconds > 10)
-        {
-            _timerCounter.color = Color.yellow;
-        }else if(_timerInSeconds < 10)
-        {
-            _timerCounter.color = Color.red;
-        }
-        _timerCounter.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        OnTimerUpdate();
+        _pointsCounter.text = _points.ToString();
     }
 
     #region ACTIONS
@@ -52,6 +36,30 @@ public class GameManager : MonoBehaviour
 
         _gameOverMessage.text = isVictory ? "VICTORIA!!" : "DERROTA";
         _gameOverMessage.color = isVictory ? Color.cyan : Color.red;
+    }
+
+    private void OnTimerUpdate()
+    {
+        if (_timerInSeconds > 0)
+        {
+            _timerInSeconds -= Time.deltaTime;
+        }
+        else if (_timerInSeconds < 0)
+        {
+            _timerInSeconds = 0;
+            EventsManager.instance.EventGameOver(true);
+        }
+        int minutes = Mathf.FloorToInt(_timerInSeconds / 60);
+        int seconds = Mathf.FloorToInt(_timerInSeconds % 60);
+        if (_timerInSeconds < 30 && _timerInSeconds > 10)
+        {
+            _timerCounter.color = Color.yellow;
+        }
+        else if (_timerInSeconds < 10)
+        {
+            _timerCounter.color = Color.red;
+        }
+        _timerCounter.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
     #endregion
 }
